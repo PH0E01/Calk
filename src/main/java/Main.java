2a7193ae512dc8c8584b0ef3a72846d2b280ffad
PH0E01 <phoetears01@gmail.com>
2024-06-23 01:51:17 +0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Ints calc = new IntsCalculator();
+        //демонстрация
+        System.out.println(calc.sum(2, 6));
+        System.out.println(calc.sum(20, 21));
+        System.out.println(calc.pow(2, 15));
+    }
+}
